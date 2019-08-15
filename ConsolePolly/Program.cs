d5dc77b882f0271ec25595019155fdd8ec1add83
Artur Skate <arturskateeveryday@hotmail.com>
2019-08-15 08:24:15 -0300

    Minor adjustments

--- ConsolePolly/Program.cs
+++ ConsolePolly/Program.cs
@@ -19,9 +19,10 @@ namespace ConsolePolly
             var httpClient = new HttpClient();
             var response = await Policy
                 .HandleResult<HttpResponseMessage>(message => !message.IsSuccessStatusCode)
-                .WaitAndRetryAsync(10, i => TimeSpan.FromSeconds(2), (result, timeSpan, retryCount, context) =>
+                
+                .WaitAndRetryAsync(50, i => TimeSpan.FromSeconds(2), (result, timeSpan, retryCount, context) =>
                 {
-                    logger.Warning($"API is not working. Request failed with {result.Result.StatusCode}. Waiting {timeSpan} before next retry. Retry attempt {retryCount}");
+                    logger.Warning($"Zurich API is not working. Request failed with {result.Result.StatusCode}. Waiting {timeSpan} before next retry. Retry attempt {retryCount}");
                 })
                 .ExecuteAsync(() => httpClient.GetAsync("https://demo5823023.mockable.io/"));
 
